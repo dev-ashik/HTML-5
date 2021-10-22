83837f0d672cc63d2ee11b399349ced890ba202c
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-10-22 12:29:38 +0800

    form attributes done

--- php/test1.php
+++ php/test1.php
@@ -1,3 +1,8 @@
 <?php
-
+    
+    $firstname = $_GET['firstname'];
+    $lastname = $_GET['lastname'];
+    
+    echo $firstname," ", $lastname;
+    
 ?>
--- php/test2.php
+++ php/test2.php
@@ -1,2 +1,6 @@
 <?php
+    $firstname = $_GET['firstname'];
+    $lastname = $_GET['lastname'];
+    
+    echo $firstname," ", $lastname;
 ?>
